57e7851630ab63cff2d9c58f473145f1596bef09
jerluc <jeremyalucas@gmail.com>
2017-10-31 10:15:43 -0700

    [Hotfix] Ensuring temp directory is removed

--- landgrab/format/shp.py
+++ landgrab/format/shp.py
@@ -1,8 +1,8 @@
 import os
 import shapefile
+import shutil
 import tempfile
 import zipfile
-from StringIO import StringIO
 
 
 def _find_shp(d):
@@ -16,14 +16,16 @@ def _find_shp(d):
 
 def deserialize(raw):
     outfolder = tempfile.mkdtemp()
-    buf = StringIO(raw)
-    with zipfile.ZipFile(buf) as zf:
-        zf.extractall(outfolder)
-    shpfn = _find_shp(outfolder)
-    shp = shapefile.Reader(shpfn)
-    fields = [field[0] for field in shp.fields[1:]]
-    for shape_record in shp.iterShapeRecords():
-        yield {
-            'geometry': shape_record.shape.__geo_interface__,
-            'properties': dict(zip(fields, shape_record.record))
-        }
+    try:
+        with zipfile.ZipFile(raw) as zf:
+            zf.extractall(outfolder)
+        shpfn = _find_shp(outfolder)
+        shp = shapefile.Reader(shpfn)
+        fields = [field[0] for field in shp.fields[1:]]
+        for shape_record in shp.iterShapeRecords():
+            yield {
+                'geometry': shape_record.shape.__geo_interface__,
+                'properties': dict(zip(fields, shape_record.record))
+            }
+    finally:
+        shutil.rmtree(outfolder)
