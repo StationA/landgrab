58dd9a1bbe1a35afa29a1f0eb00b854082bff7c1
Manos Saratsis <manos.saratsis@gmail.com>
2017-12-05 15:12:36 -0800

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ TESTS_REQUIRE = requirements('./test_requirements.txt')
 
 setup(
     name='landgrab',
-    version='0.1.4',
+    version='0.1.5',
     description='Geospatial data hoarding system',
     author='Station A',
     author_email='software@stationa.com',
